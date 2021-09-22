0f173ef7ed3f4271d27f9bd89a3a39a4c0ee7e8c
fahadimaduddin <fahaduddin1997@gmail.com>
2021-09-22 21:16:38 +0500

    initial commit

---
+++ EFCoreCodeFirst/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirst.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+    }
+}
